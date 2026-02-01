a16865df217fcf49da5ffd970b207c02d5082cba
mohan <mohanli202409@gmail.com>
2026-01-31 21:08:16 -0500

    feat: updating java..

--- java_practice/BinaryTreeLevelOrderTraversal.java
+++ java_practice/BinaryTreeLevelOrderTraversal.java
@@ -1,5 +1,53 @@
 package java_practice;
 
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
 public class BinaryTreeLevelOrderTraversal {
-    
+    public static List<List<Integer>> levelOrder(TreeNode root){
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+        // use a queue
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if(node.left!= null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(levelOrder(root));
+
+    }
+
 }
--- java_practice/ProductofArrayExceptSelf.java
+++ java_practice/ProductofArrayExceptSelf.java
@@ -1,7 +1,33 @@
 package java_practice;
 
+import java.util.Arrays;
+
 public class ProductofArrayExceptSelf {
-    
-    
-    
+    public static int[] Product(int[] nums){
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        for(int i = 1; i < nums.length; i++){
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int suffix = 1;
+        for(int i = nums.length - 1; i >= 0; i--){
+            res[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args){
+        int[][] testcases = {
+            {0},
+            {1},
+            {1,2,3,4},
+            {-1,1,0,-3,3}
+        };
+
+        for(int[] testcase : testcases){
+            System.out.println(Arrays.toString(testcase) + ">>>>>>>>" + Arrays.toString(Product(testcase)));
+        }
+    }
+
 }
